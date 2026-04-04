de859c6fcd4923653b6e1584de2dbaa69b43a4de
Prajwal Hendre <167730711+prajwalhendre@users.noreply.github.com>
2026-04-04 10:26:53 -0600

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen = {}
+
+        for letter in s:
+            seen[letter] = seen.get(letter, 0) + 1
+        
+        for letter in t:
+            seen[letter] = seen.get(letter, 0) - 1
+
+        return all(val == 0 for val in seen.values()) 
